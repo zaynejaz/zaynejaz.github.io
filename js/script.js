e6f3bee72cac0db5f868e38e978a46c047ac2a56
zejaz <zayn.ejaz@gmail.com>
2020-04-20 20:36:55 +0300

    JS to external from embedded

--- js/script.js
+++ js/script.js
@@ -20,3 +20,36 @@ if (!console) {
 	};
 	console.log(calculate_age(new Date(1986, 05, 23)));
 
+// +++++++++++++++++=========================+++++++++++++++++++++
+// MODAL POPUP
+// modal
+var modal = document.getElementById("myModal");
+// button opens modal
+var btn = document.getElementById("myBtn");
+// <span> element closes modal
+var span = document.getElementsByClassName("close")[0];
+// clicks button = open modal 
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+// clicks <span> (x) = close modal
+span.onclick = function() {
+	modal.style.display = "none";
+}
+// clicks outside modal = close modal
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+  	}
+}
+	
+// +++++++++++++++++=========================+++++++++++++++++++++
+// CURSOR
+"use strict";
+	var cursor = document.querySelector("#cursor");
+	var cursorListener = function cursorListener(event){
+		cursor.style.top = event.y + "px";
+		cursor.style.left = event.x + "px";
+	};
+
+	window.addEventListener("mousemove", cursorListener);
