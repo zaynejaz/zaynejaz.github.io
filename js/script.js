0cb93a7cef929e2bc206bf870f96d4598191930e
Zayn Ejaz <zayn.ejaz@gmail.com>
2020-11-29 21:00:26 +0300

    commented out age

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 // CALCULATE AGE
-function calculate_age(dob) { 
+/* function calculate_age(dob) { 
 
-	var diff_ms = Date.now() - dob.getTime();
-	var age_dt = new Date(diff_ms); 
+ 	var diff_ms = Date.now() - dob.getTime();
+ 	var age_dt = new Date(diff_ms); 
 
-	return Math.abs(age_dt.getUTCFullYear() - 1970);
-}
-if (!console) {
-	console = {};
-}
-	var old = console.log;
-	var logger = document.getElementById('myAge');
-	console.log = function (message) {
-		if (typeof message == 'object') {
-			logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
-		} else {
-			logger.innerHTML += message + '<br />';
-		}
-	};
-	console.log(calculate_age(new Date(1986, 05, 23)));
+ 	return Math.abs(age_dt.getUTCFullYear() - 1970);
+ }
+ if (!console) {
+ 	console = {};
+ }
+ 	var old = console.log;
+ 	var logger = document.getElementById('myAge');
+ 	console.log = function (message) {
+ 		if (typeof message == 'object') {
+ 			logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
+ 		} else {
+ 			logger.innerHTML += message + '<br />';
+ 		}
+ 	};
+ 	console.log(calculate_age(new Date(1986, 05, 23)));*/
 
 // +++++++++++++++++=========================+++++++++++++++++++++
 // MODAL POPUP
